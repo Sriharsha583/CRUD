a6e8441e088ba54638e573147c0837bc10c5724f
Sriharsha583 <112861642+Sriharsha583@users.noreply.github.com>
2022-09-05 15:11:46 +0530

    Add files via upload

--- student.js
+++ student.js
@@ -1,18 +1,33 @@
-const mongoose=require('mongoose')
-const stuschema=mongoose.Schema({
-    rollno:{
-        type:String,
-        required:true
-    },
-    name:{
-        type:String,
-        required:true
-    },
-    scholarship:{
-        type:String,
-        required:true,
-        default:false
+const express=require('express')
+
+const router=new express.Router()
+const student1=require('../models/student')
+
+router.get('/',async(req,res)=>{
+    try{
+        const s1=await student1.find()
+        res.json(s1)
+    }catch(err){
+        res.send('error'+err)
+
+    }
+})
+
+
+router.post('/',async(req,res)=>{
+    const Student1=new student1({
+        rollno:req.body.rollno,
+        name:req.body.name,
+        scholarship:req.body.scholarship
+    })
+    try{
+        const s2=await Student1.save()
+        res.json()
+        
+1
+    }catch(err){
+        res.send('error'+err)
     }
 
 })
-module.exports=mongoose.model('student1',stuschema)
+module.exports=router
